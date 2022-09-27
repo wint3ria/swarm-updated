5b4d104e79eec2e97da41d84bc458fe7bed3d4df
Nicolas Misk <nicolas.misk@gmail.com>
2022-09-27 10:08:58 +0200

    Improve logging and configuration

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-import { readFile, watch } from "fs/promises";
+import { readFile, watch, readdir, utimes } from "fs/promises";
 import config from 'config';
 import Docker from 'dockerode';
+import { createLogger, config as wconfig, transports, format } from 'winston';
+
+const { combine } = format;
+const logger = createLogger({
+  levels: wconfig.npm.levels,
+  transports: [
+    ...(config.has("logging.transport.console") ?
+          config.get("logging.transport.console").map(cfg => new transports.Console(cfg))
+        : []
+    ),
+    ...(config.has("logging.transport.File") ?
+          config.get("logging.transport.file").map(cfg => new transports.File(cfg))
+        : []
+    ),
+  ],
+  exitOnError: config.get("logging.exitOnError"),
+  format: combine(
+    ...config.get("logging.formatters").map(key => format[key]())
+  )
+})
+
+function later(delay) {
+  return new Promise(function(resolve) {
+      setTimeout(resolve, delay);
+  });
+}
+
+function generate_name(namespace, folder_path, filename) {
+  const part1 = folder_path.trim()
+    .replace(" ", "_")
+    .replace("/", "_")
+    .replace(".", "")
+    .replace(/^_+|_+$/gm,'');
+  const part2 = filename.trim()
+    .replace(" ", "_")
+    .replace("/", "_")
+    .replace(".", "_")
+    .replace(/^_+|_+$/gm,'');
+  const name = namespace + "_" + part1 + "_" + part2
+  return name
+}
 
 const secrets_folder = config.get("secret_folder")
 
 const docker = new Docker(config.get("docker"));
+
+const fileTouchDelay = config.get("fileTouchDelay")
 
 const secrets_register = {}
 
@@ -12,7 +55,8 @@ async function updateServiceSecret(oldSecret, newSecretId, serviceResponse, vers
   const oldSecretTarget = serviceResponse.Spec.TaskTemplate.ContainerSpec.Secrets.find(
     s => s.SecretID === oldSecret.ID
   )
-  console.log(oldSecretTarget)
+  const oldSecretName = oldSecretTarget.SecretName
+  const newSecretName = oldSecretName.split(".")[0] + "." + version
   const newSecrets = [
     ...serviceResponse.Spec.TaskTemplate.ContainerSpec.Secrets.filter(
       s => s.SecretID !== oldSecret.ID
@@ -20,13 +64,17 @@ async function updateServiceSecret(oldSecret, newSecretId, serviceResponse, vers
     {
       File: oldSecretTarget.File,
       SecretID: newSecretId,
-      SecretName: oldSecretTarget.SecretName.split(".")[0] + "." + version
+      SecretName: newSecretName
     }
   ]
   const service = docker.getService(serviceResponse.ID)
-
   try {
-    console.log("Update service", serviceResponse.Spec.Name, "using outdated secret", oldSecret.Spec.Name)
+    logger.info(
+      "Updating service %s using secret %s to its new version %s",
+      serviceResponse.Spec.Name,
+      oldSecretName,
+      newSecretName
+    )
     await service.update("auth", {
       ...serviceResponse.Spec,
       version: serviceResponse.Version.Index,
@@ -53,16 +101,19 @@ async function dockerSecretsUpdate (namespace, secret_name, folder_path, filenam
   if (existingSecret === undefined) {
     const labels = {};
     labels[versionLabelName] = "0"
-    console.log(secret_name, "does not exist, creating from", path)
+    logger.info("%s does not exist, creating from %s", secret_name, path)
     await docker.createSecret({Name: secret_name, Labels: labels, Data: content})
   } else {
-    console.log(secret_name, "exists, updating from", path)
+    logger.info("%s exists, updating from %s", secret_name, path)
     const labels = {};
     const secret = docker.getSecret(existingSecret.ID)
-    const labelVersion = String(parseInt(existingSecret.Spec.Labels[versionLabelName]) + 1)
+    const oldLabelVersion = existingSecret.Spec.Labels[versionLabelName]
+    const labelVersion = String(parseInt(oldLabelVersion) + 1)
+    logger.info("Updating secret %s from version %s to version %s", secret_name, oldLabelVersion, labelVersion)
     labels[versionLabelName] = labelVersion
     const config = {Name: secret_name + "." + labelVersion, Labels: labels, Data: content}
     const newSecretId = (await docker.createSecret(config)).id
+    logger.info("Updating services using old secret %s.%s", secret_name, oldLabelVersion)
     const updatePromises = (await docker.listServices())
       .filter(resp => resp.Spec.TaskTemplate.ContainerSpec.Secrets)
       .filter(resp =>
@@ -80,43 +131,44 @@ async function dockerSecretsUpdate (namespace, secret_name, folder_path, filenam
   }
 }
 
-function generate_name(namespace, folder_path, filename) {
-  const part1 = folder_path
-    .replace("/", "_")
-    .replace(".", "")
-
-  const part2 = filename
-    .replace("/", "_")
-    .replace(".", "_")
-
-  return namespace + "_" + part1 + "_" + part2
-}
-
 async function file_event_handler(register, namespace, folder_path, eventType, filename) {
-  console.log("Event", eventType, "on file", filename)
+  logger.info("Event %s happening on file %s", eventType, filename)
   const secret_name = generate_name(namespace, folder_path, filename);
   const isChange = eventType === "change"
   const alreadyRegistered = secret_name in register
   if (!alreadyRegistered) {
-    console.log("Configuration", secret_name, "first registration, activated:", isChange)
+    logger.info("Secret %s first registration, activated: %s", secret_name, isChange)
   }
   if (!isChange) {
-    console.log("Configuration", secret_name, "deactivation")
+    logger.info("Secret deactivation: %s", secret_name)
   }
   const activeOld = register[secret_name]
   const newActive = (activeOld || !alreadyRegistered) && isChange
   if (!newActive) {
-    console.log("Configuration", secret_name, "deactivated")
+    logger.info("Configuration %s deactivated", secret_name)
     return;
   }
   register[secret_name] = newActive
   if (newActive) {
-    console.log("Configuration", secret_name, "is active, performing docker operations")
+    logger.info("Secret %s is active, performing docker operations", secret_name)
     dockerSecretsUpdate(namespace, secret_name, folder_path, filename)
   }
 }
 
+async function scheduleTouchFiles(folder_path) {
+  const files = await readdir(folder_path)
+  logger.info("Folder %s contains the following files: %s", folder_path, files)
+  await Promise.all(files.map(async filename => {
+    logger.info("Waiting %d ms to touch file %s/%s", fileTouchDelay, folder_path, filename)
+    await later(fileTouchDelay)
+    logger.info("Touching file %s/%s", folder_path, filename)
+    const now = new Date()
+    await utimes(folder_path + "/" + filename, now, now)
+  }))
+}
+
 async function configure(namespace, folder_path) {
+  logger.info("Configuring namespace %s, using folder %s", namespace, folder_path)
   const watcher = watch(folder_path)
   for await (const event of watcher) {
     const { eventType, filename } = event
@@ -124,10 +176,19 @@ async function configure(namespace, folder_path) {
   }
 }
 
+logger.info("Launching Swarm Updated")
+
 Promise.all(Object.keys(secrets_folder).map(
-  async namespace => configure(
+  async namespace => await configure(
     namespace,
     secrets_folder[namespace]
   )
 ))
   .catch(err => console.error("Could not launch configuration", err))
+
+logger.info("Scheduling files touch")
+
+Promise.all(Object.keys(secrets_folder).map(
+  async namespace => await scheduleTouchFiles(secrets_folder[namespace])
+))
+  .catch(err => console.error("Could not schedule touch files", err))
